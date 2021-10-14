af08b7fcf06132bf90edb94b05eeacf478a9491f
Eddishot <Eddi_sl94@hotmail.it>
2021-10-14 21:05:42 +0200

    commit

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+
+
+// creo un let per il container
+let myContainer = document.querySelector(".mycontainer");
+
+// creo un array per le immagini
+
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+]
+
